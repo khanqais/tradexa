b9bb57e9648705589f05b80300eb044ef08b8a84
ayaz1017 <khanzakiya6600@gmail.com>
2026-03-07 16:03:35 +0530

    localhost

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: 3000,
     proxy: {
       // Proxy REST API calls
       '/api': {
